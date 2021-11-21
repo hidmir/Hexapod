c1b0a257ee2d8829d563c6fc25f800c9fc631143
Jakub Miętus <jakub.mietus@4experience.co>
2021-11-22 00:08:25 +0100

    WIP - moving movement logic from Leg class to Hexapod class.

--- Hexapod_Praca_Inzynierska.ino
+++ Hexapod_Praca_Inzynierska.ino
@@ -7,6 +7,7 @@ Adafruit_PWMServoDriver secondPWMDriver = Adafruit_PWMServoDriver(0x41);
 #define SERVOMIN  100//150
 #define SERVOMAX  485//600
 #define SERVO_FREQ 50
+#define NULL_VALUE -1
 
 void rotate(uint8_t servoDriverNumber, uint8_t servoNumber, uint16_t angleInDegrees)
 {
@@ -22,39 +23,65 @@ void rotate(uint8_t servoDriverNumber, uint8_t servoNumber, uint16_t angleInDegr
   }
 }
 
+enum Side
+{
+  RIGHT,
+  LEFT
+};
+
+enum PartType
+{
+  LOWER,
+  UPPER,
+  CONNECTOR
+};
+
+enum MovementType
+{
+  SET_HOME_POSITION,
+  SET_DEFAULT_POSITION,
+  SET_NEUTRAL_POSITION,
+  RISE_LEG,
+  LOWER_LEG,
+  ROTATE_FORWARD,
+  ROTATE_BACKWARD
+};
+
+class Part
+{
+  public:
+  uint8_t driverNumber;
+  uint8_t partNumber;
+  int16_t currentAngle;
+  int16_t targetAngle;
+
+  Part(){ }
+  
+  Part(PartType partType, uint8_t numberOfPart)
+  {
+    driverNumber = partType == CONNECTOR ? 2 :1 ;
+    partNumber = numberOfPart;
+  }
+ };
+
 class Leg 
 {
-  class Part
-  {
-    public:
-    uint8_t driverNumber;
-    uint8_t partNumber;
-    int16_t currentAngle;
-    int16_t targetAngle;
-
-    Part(){ }
-    
-    Part(uint8_t numberOfDriver, uint8_t numberOfPart)
-    {
-      driverNumber = numberOfDriver;
-      partNumber = numberOfPart;
-    }
-  };
-  
   public:
+    Part partsCollection[3];
+    
     Leg() { }
     
     Leg(uint8_t lowerPartServoNumber, uint8_t upperPartServoNumber, uint8_t legsConnectorServoNumber)
     {
-      partsCollection[0] = Part(1, lowerPartServoNumber);
-      partsCollection[1] = Part(1, upperPartServoNumber);
-      partsCollection[2] = Part(2, legsConnectorServoNumber);
+      partsCollection[0] = Part(LOWER, lowerPartServoNumber);
+      partsCollection[1] = Part(UPPER, upperPartServoNumber);
+      partsCollection[2] = Part(CONNECTOR, legsConnectorServoNumber);
     }
     
     void riseLeg(uint8_t numberOfIterations)
     {
-      partsCollection[0].targetAngle = 60;
-      partsCollection[1].targetAngle = 60;
+      partsCollection[0].targetAngle = 50;
+      partsCollection[1].targetAngle = 50;
 
       rotatePartsAsynchronously(numberOfIterations);
       saveCurrentPosition();
@@ -62,9 +89,9 @@ class Leg
     
     void lowerLeg(uint8_t numberOfIterations)
     {
-      partsCollection[0].targetAngle = 120;
-      partsCollection[1].targetAngle = 120;
-      
+      partsCollection[0].targetAngle = 100;
+      partsCollection[1].targetAngle = 100;
+
       rotatePartsAsynchronously(numberOfIterations);
       saveCurrentPosition();
     }
@@ -84,11 +111,20 @@ class Leg
       rotatePartsAsynchronously(numberOfIterations);
       saveCurrentPosition();
     }
+
+    void setHomePosition(uint8_t numberOfIterations)
+    {
+      partsCollection[0].targetAngle = 30;
+      partsCollection[1].targetAngle = 0;
+
+      rotatePartsAsynchronously(numberOfIterations);
+      saveCurrentPosition();
+    }
     
     void setDefaultPosition(uint8_t numberOfIterations)
     {
-      partsCollection[0].targetAngle = 90;
-      partsCollection[1].targetAngle = 90;
+      partsCollection[0].targetAngle = 100;
+      partsCollection[1].targetAngle = 100;
       partsCollection[2].targetAngle = 90;
       
       rotatePartsAsynchronously(numberOfIterations);
@@ -101,7 +137,7 @@ class Leg
       {
         rotate(partsCollection[partIndex].driverNumber, partsCollection[partIndex].partNumber, 90);
         partsCollection[partIndex].currentAngle = 90;
-        delay(1500);
+        delay(400);
       }
     }
     
@@ -109,7 +145,6 @@ class Leg
     uint8_t lowerPart;
     uint8_t upperPart;
     uint8_t legsConnector;
-    Part partsCollection[3];
     
     void rotatePartsAsynchronously(uint8_t numberOfIterations)
     {
@@ -143,49 +178,124 @@ class Leg
 class Hexapod
 {
   public:
-    Hexapod(Leg firstLeg, Leg secondLeg, Leg thirdLeg, Leg fourthLeg, Leg fifthLeg, Leg sixthLeg)
+    Hexapod(Leg firstRightLeg, Leg secondRightLeg, Leg thirdRightLeg, Leg firstLeftLeg, Leg secondLeftLeg, Leg thirdLeftLeg)
     {
-      firstRightLeg = firstLeg;
-      secondRightLeg = secondLeg;
-      thirdRightLeg = thirdLeg;
-      firstLeftLeg = fourthLeg;
-      secondLeftLeg = fifthLeg;
-      thirdLeftLeg = sixthLeg;
-    }
-
-    void setNeutralPosition()
-    {
-      firstRightLeg.setNeutralPosition();
-      secondRightLeg.setNeutralPosition();
-      thirdRightLeg.setNeutralPosition();
-      firstLeftLeg.setNeutralPosition();
-      secondLeftLeg.setNeutralPosition();
-      thirdLeftLeg.setNeutralPosition();
+      legsCollection[0] = firstRightLeg;
+      legsCollection[1] = secondRightLeg;
+      legsCollection[2] = thirdRightLeg;
+      legsCollection[3] = firstLeftLeg;
+      legsCollection[4] = secondLeftLeg;
+      legsCollection[5] = thirdLeftLeg;
     }
 
     void moveForward()
     {
-      firstRightLeg.riseLeg(60);
-      secondRightLeg.riseLeg(60);
+      //firstRightLeg.riseLeg(60);
+      /*secondRightLeg.riseLeg(60);
       thirdRightLeg.riseLeg(60);
       firstLeftLeg.riseLeg(60);
       secondLeftLeg.riseLeg(60);
-      thirdLeftLeg.riseLeg(60);
-      firstRightLeg.lowerLeg(60);
-      secondRightLeg.lowerLeg(60);
+      thirdLeftLeg.riseLeg(60);*/
+      //firstRightLeg.lowerLeg(60);
+      /*secondRightLeg.lowerLeg(60);
       thirdRightLeg.lowerLeg(60);
       firstLeftLeg.lowerLeg(60);
       secondLeftLeg.lowerLeg(60);
-      thirdLeftLeg.lowerLeg(60);
+      thirdLeftLeg.lowerLeg(60);*/
+    }
+
+    void setNeutralPosition()
+    {
+      /*firstRightLeg.setNeutralPosition();
+      secondRightLeg.setNeutralPosition();
+      thirdRightLeg.setNeutralPosition();
+      firstLeftLeg.setNeutralPosition();
+      secondLeftLeg.setNeutralPosition();
+      thirdLeftLeg.setNeutralPosition();*/
+    }
+
+    void setHomePosition()
+    {
+      //firstRightLeg.setHomePosition(60);
     }
 
   private:
-    Leg firstRightLeg;
-    Leg secondRightLeg;
-    Leg thirdRightLeg;
-    Leg firstLeftLeg;
-    Leg secondLeftLeg;
-    Leg thirdLeftLeg;
+    Leg legsCollection[6];
+
+    void setTargetAngles(MovementType movementType)
+    {
+      switch(movementType)
+      {
+        case SET_HOME_POSITION:
+        {
+          setAngles(30, 0, 90);
+        } break;
+        case SET_DEFAULT_POSITION:
+        {
+          setAngles(100, 100, 90);
+        } break;
+        case SET_NEUTRAL_POSITION:
+        {
+          setAngles(90, 90, 90);
+        } break;
+        case RISE_LEG:
+        {
+          setAngles(50, 50, NULL_VALUE);
+        } break;
+        case LOWER_LEG:
+        {
+          setAngles(100, 100, NULL_VALUE);
+        } break;
+        case ROTATE_FORWARD:
+        {
+          setAngles(NULL_VALUE, NULL_VALUE, 120);
+        } break;
+        case ROTATE_BACKWARD:
+        {
+          setAngles(NULL_VALUE, NULL_VALUE, 60);
+        } break;
+      }
+    }
+
+    void setAngles(uint8_t lowerPartTargetAngle, uint8_t upperPartTargetAngle, uint8_t connectorPartTargetAngle)
+    {
+      for(uint8_t legIndex = 0; legIndex < 6; legIndex++)
+      {
+        legsCollection[legIndex].partsCollection[0].targetAngle = lowerPartTargetAngle != NULL_VALUE ? lowerPartTargetAngle : legsCollection[legIndex].partsCollection[0].currentAngle;
+        legsCollection[legIndex].partsCollection[1].targetAngle = upperPartTargetAngle != NULL_VALUE ? upperPartTargetAngle : legsCollection[legIndex].partsCollection[1].currentAngle;
+        legsCollection[legIndex].partsCollection[2].targetAngle = connectorPartTargetAngle != NULL_VALUE ? connectorPartTargetAngle : legsCollection[legIndex].partsCollection[2].currentAngle;
+      }
+    }
+    
+    void rotateLegsAsynchronously(uint8_t numberOfIterations)
+    {
+      uint8_t targetAngle;
+      int16_t partTargetAngle;
+      int16_t partCurrentAngle;
+      Part partsCollection[3];
+      Part part;
+      Leg leg;
+
+      for(uint8_t iteration = 1; iteration <= numberOfIterations; iteration++)
+      {
+        for(uint8_t legIndex = 0; legIndex < 6; legIndex++)
+        {
+          leg = legsCollection[legIndex];
+          
+          for(uint8_t partIndex = 0; partIndex < 3; partIndex++)
+          {
+            part = partsCollection[partIndex];
+            partTargetAngle = (int16_t)leg.partsCollection[partIndex].targetAngle;
+            partCurrentAngle = (int16_t)leg.partsCollection[partIndex].currentAngle;
+            targetAngle = partCurrentAngle + (((((partTargetAngle - partCurrentAngle)* 100) / numberOfIterations) * iteration) / 100);
+            targetAngle = (uint8_t)targetAngle;
+            Serial.println(targetAngle);
+            rotate(leg.partsCollection[partIndex].driverNumber, leg.partsCollection[partIndex].partNumber, targetAngle);
+            delay(10);
+          }
+        }
+      }
+    }
 };
 
 Leg firstLeg(0, 1, 0);
@@ -205,11 +315,12 @@ void setup() {
   firstPWMDriver.setPWMFreq(SERVO_FREQ);
   secondPWMDriver.setPWMFreq(SERVO_FREQ);
 
-  hexapod.setNeutralPosition();
+  //hexapod.setNeutralPosition();
 
   delay(10);
 }
 
 void loop() {
-  hexapod.moveForward();
+  //hexapod.moveForward();
+  //hexapod.setHomePosition();
 }
